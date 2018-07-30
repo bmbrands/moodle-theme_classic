5df7899db6b8755445c59323ec9668563a8701a8
Bas Brands <bas@moodle.com>
2018-07-30 10:26:30 +0200

    Theme classic Alpha

---
+++ README.txt
@@ -0,0 +1,13 @@
+This is a child theme of the Boost theme that replaces the clean theme.
+
+It supports:
+
+Bootstrap 4 Sass and JavaScript
+The 1, 2 and 3 column grid
+The settings and navigation blocks
+Allow the use of "classic" presets
+
+It does not support:
+
+docking
+css written in less
---
+++ version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of the classic theme for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * classic backgrounds.
+ *
+ * @package    theme_classics
+ * @copyright  2018 Bas Brands
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2018073000;
+$plugin->requires = 2018051700;
+$plugin->component = 'theme_classic';
+$plugin->dependencies = [
+    'theme_boost' => 2018051400
+];
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 1.1;
+
